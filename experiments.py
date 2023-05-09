33f0853e9dad28b889726b69bf59ab00b76caa56
tau_chang <tomy.1516@gmail.com>
2023-05-09 02:11:33 -0500

    Modify experiment

--- experiments.py
+++ experiments.py
@@ -9,33 +9,21 @@ def hop_expirement(users):
         end_time = time.time()
         elapsed_time[node].append(end_time - start_time)
     
-    def issueHasBlob(node, blob_id, elapsed_time):
-        start_time = time.time()
-        # loop until node has blob
-        sleep_interval = 0.1
-        while not users[node].hasBlob(blob_id):
-            time.sleep(sleep_interval)
-            # sleep_interval = min(sleep_interval+1, 0.5)
-        print(f"{node} has blob")
-        end_time = time.time()
-        elapsed_time[node].append(end_time - start_time)
-    
-    users["node-1"].follow(users["node-0"])
-    users["node-2"].follow(users["node-0"])
-    users["node-3"].follow(users["node-0"])
-    users["node-4"].follow(users["node-0"])
-    users["node-5"].follow(users["node-0"])
+    publisher = "node0"
+    recipients = users.keys() - [publisher]
+
+    for node in recipients:
+        users[node].follow(users[publisher])
 
     elapsed_time = {}
-    recipients = users.keys() - ["node-0"]
     for node in recipients:
         elapsed_time[node] = []
 
-    for i in range(5):
+    for i in range(1):
         print("round: ", i)
         fname = f"file{i}"
-        blob_id = users["node-0"].addBlob(fname, 1000000) # max 5 mb
-        users["node-0"].pushBlob(blob_id)
+        blob_id = users[publisher].addBlob(fname, 1000000) # max 5 mb
+        users[publisher].pushBlob(blob_id)
 
         # issue wantsBlob for all nodes in parallel threads
         threads = []
@@ -49,19 +37,6 @@ def hop_expirement(users):
         
         for thread in threads:
             thread.join()
-    
-    # selected_index = []
-    # # pick the 5 indices with smallest time for node-3
-    # for i in range(5):
-    #     min_time = sys.maxsize
-    #     min_index = -1
-    #     for index, t in enumerate(elapsed_time["node-1"]):
-    #         if t < min_time and index not in selected_index:
-    #             min_time = t
-    #             min_index = index
-    #     selected_index.append(min_index)
-    
-    # print(selected_index)
     
     for node in recipients:
         # elapsed_time[node] = [elapsed_time[node][i] for i in selected_index]
